692fdddd6871e9f10cce1691c4e338cc6c155188
Hudson Nguyen <hungson.nguyen811@gmail.com>
2023-09-23 19:42:37 -0500

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { CounterProvider } from './context/CounterContext'
 import Counter from './components/Counter'
-import { ServicesProvider } from './context/ServicesProvider'
 
 function App() {
   return (
     <>
-      <CounterProvider>
-        <ServicesProvider>
-          <Counter />
-        </ServicesProvider>
-      </CounterProvider>
+      <Counter />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { CounterProvider } from './context/CounterContext'
+import { ServicesProvider } from './context/ServicesProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CounterProvider>
+      <ServicesProvider>
+        <App />
+      </ServicesProvider>
+    </CounterProvider>
   </React.StrictMode>,
 )
