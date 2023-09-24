82db0c11088bda4a5563607eb93dd6071def03f2
Hudson Nguyen <hungson.nguyen811@gmail.com>
2023-09-24 10:51:22 -0500

    added search and filter service

--- src/pages/Farmer/ServiceItem.tsx
+++ src/pages/Farmer/ServiceItem.tsx
@@ -12,23 +12,23 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
   handleStartEditing,
 }) => {
   return (
-    <div className='p-4 mb-4 border rounded-md'>
+    <div className='p-4 mb-4 text-center border rounded-md hover:bg-gray-50'>
       <h3 className='text-lg font-semibold'>{service.title}</h3>
       <p className='text-gray-600'>{service.description}</p>
       <p className='text-blue-500'>${service.price}</p>
       <p className='text-gray-500'>{service.location}</p>
       <div>
-        <div className='inline-flex rounded-md shadow-sm' role='group'>
+        <div className='inline-flex m-2 rounded-md shadow-sm' role='group'>
           <button
             type='button'
-            className='px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700'
+            className='px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-blue-400 hover:text-white focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700'
             onClick={() => handleStartEditing(service.id)}
           >
             Edit
           </button>
           <button
             type='button'
-            className='px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-r border-gray-200 rounded-r-lg hover:bg-red-500 hover:text-white focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700'
+            className='px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-r border-gray-200 rounded-r-lg hover:bg-red-400 hover:text-white focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700'
             onClick={() => handleDelete(service.id)}
           >
             Delete
