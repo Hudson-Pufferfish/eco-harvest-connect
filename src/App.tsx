06d1fee22c3889e746a402eaf59ba2aea2969017
Hudson Nguyen <hungson.nguyen811@gmail.com>
2023-09-23 20:02:04 -0500

    handled conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
-import Counter from './components/Counter'
-=======
-
 import CreateService from './components/CreateService'
->>>>>>> Stashed changes
 
 function App() {
   return (
     <>
-<<<<<<< Updated upstream
-      <Counter />
-=======
       <CreateService />
->>>>>>> Stashed changes
     </>
   )
 }
